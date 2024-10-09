2cc5e38f1cf05835645cc5f57ae91812edb023cf
Shavaizz <shaviz4444@gmail.com>
2024-10-09 20:52:51 +0500

    Initial Commit

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header(){
+    return(
+        <>
+            <section className="logoAndHeader">
+                <div id="logo">
+                    <h2>
+                        Password Generator
+                    </h2>
+                </div>
+                <div id="headerlink">
+                    <p><a href="github.com/Shavaizz">See More Projects</a></p>
+                </div>
+            </section>
+        </>
+    )
+}
